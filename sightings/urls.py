7744f2911ab303ee76ee40d7d6dd468343e39ecd
jingtristaxiao <59104162+jingtristaxiao@users.noreply.github.com>
2020-10-21 01:24:31 +0800

    Remove map from sightings

--- sightings/urls.py
+++ sightings/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
     path('stats/', views.stats, name='stats'),
-    path('<squirrel_id>/', views.detail, name='detail'),
-    path('map/',views.show_map, name='show_map')
+    path('<squirrel_id>/', views.detail, name='detail')
 ]
