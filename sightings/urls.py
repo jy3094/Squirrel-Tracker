b715b6ac9ad4b1c5d9721c5f94c368808cfd64e7
jingtristaxiao <59104162+jingtristaxiao@users.noreply.github.com>
2020-10-20 11:41:30 +0800

    Modify map to HTML

--- sightings/urls.py
+++ sightings/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
     path('stats/', views.stats, name='stats'),
-    path('<squirrel_id>/', views.detail, name='detail')
+    path('<squirrel_id>/', views.detail, name='detail'),
+    path('map/',views.show_map, name='show_map')
 ]
